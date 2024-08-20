36e72dbf50cf5ea6751a2a902fae7f2da213c2ef
Vian Tran <you@example.com>
2024-08-20 19:20:53 -0400

    exploring the data

--- COVID-19_Dashboard_Project.R
+++ COVID-19_Dashboard_Project.R
@@ -2,3 +2,14 @@
 # Final Exam
 # Summer 2024
 # The Very Best Team
+
+# Load necessary packages.
+library(tidyverse)
+
+# Import data.
+COVID <- read.csv("~/TheVeryBest_CodingInR/WHO-COVID-19-global-table-data.csv")
+
+# Explore data.
+summary(COVID)
+str(COVID)
+names(COVID)
