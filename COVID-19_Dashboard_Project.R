66b72488f0e4b0af7d56c3b8f153b07e5933ccbb
Vian Tran <you@example.com>
2024-08-20 19:24:31 -0400

    create a subset of the data to include necessary variables for our objective

--- COVID-19_Dashboard_Project.R
+++ COVID-19_Dashboard_Project.R
@@ -13,3 +13,10 @@ COVID <- read.csv("~/TheVeryBest_CodingInR/WHO-COVID-19-global-table-data.csv")
 summary(COVID)
 str(COVID)
 names(COVID)
+
+# Create a subset of COVID dataset to include necessary variables such as Name, Who.Region, Cases...cumulative.total.per.100000.population,
+# and Deaths...cumulative.total.per.100000.population and rename the variables.
+COVID_clean <- COVID %>% 
+  select(Name, WHO.Region, 
+         Cases.Cumulative.Per.100k = Cases...cumulative.total.per.100000.population, 
+         Deaths.Cumulative.Per.100k = Deaths...cumulative.total.per.100000.population)
